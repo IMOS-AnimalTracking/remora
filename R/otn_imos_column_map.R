fc66bbfda0eee1793235a5faf12e6ccbbfaf30b8
Bruce Delo <bruce@ANunes.OCEAN.Dal.Ca>
2022-06-29 13:37:17 -0300

    Starting build-out of the otn_imos_column_map function that'll clobber OTN-formatted data into data that Remora likes.

---
+++ R/otn_imos_column_map.R
@@ -0,0 +1,199 @@
+#In order to save ourselves work going forward, this function takes a set of OTN-formatted data and mutates the columns in each dataframe to produce equivalencies
+#To the IMOS spec. This lets most of REMORA run without further argument- hopefully. 
+
+#Derive is just a flag for if we're working with OTN data, have a detection extract but no receiver metadata or tag metadata. This way, we can run it
+#both ways- if Derive is false, we'll knock together an IMOS-format detections dataframe and run it through Remora's built-in functionality for running
+#detections without receiver/tag metadata; if it's true, then we'll try to scuff rcvr and tag dataframes out of the detection extract file. 
+
+otn_imos_column_map <- function(det_dataframe, rcvr_dataframe = NULL, tag_dataframe = NULL, derive = TRUE) {
+  #We need to ultimately produce the following:
+  # - A detections dataframe with columns appropriate to the IMOS spec. 
+  # - A receiver dataframe with appropriate columns, if necessary with data derived from the detections dataframe.
+  # - A tag dataframe, same constraints as above.
+  # - An animal measurements dataframe with data derived from the tag dataframe.
+  
+  #At the end of all this we return three dataframes- det_data, rec_data, tag_data- that can be passed through to the merge at the bottom of the get_data function.
+  #Animal measurements is a bit weird so I'm going to ignore it for now.
+  
+  #This probably won't have the full range of columns that the equivalent IMOS data would. 
+  #Let's start with just the detections, since that's a possibility we have to account for- that we only get a detection extract and have to work
+  #outwards from there.
+  
+  #NOTE TO SELF: Remora can already handle if you don't have rcvr/tag metadata. Don't make this more complicated than it has to be. 
+  
+  #Quit instantly if there is no detections dataframe. This is unlikely since this check already happens in the function above, but for completeness'
+  #sake we'll include it. 
+  if(is.null(det_dataframe)) stop("\033[31;1mCan not run otn -> imos conversion without a detections file!\033[0m\n")
+  
+  #If we don't get passed a receiver or tag dataframe, we derive them from det. This will give us hopefully enough info that we can create the final
+  #detection dataframe to be returned, which will be valid for Remora. Ideally. 
+  if(is.null(rcvr_dataframe) && derive) {
+    rcvr_dataframe <- derive_rcvr_from_det(det_dataframe)
+  }
+  
+  if(is.null(tag_dataframe) && derive) {
+    tag_dataframe <- derive_tag_from_det(det_dataframe)
+  }
+  
+  #If we have receiver_meta, we start working with that to convert.
+  if(!is.null(rec_meta)) {
+    #Build this out once basic case is handled.
+  } else {
+    #If we don't have receiver metadata, we derive our imos-friendly detection dataframe from our otn detection dataframe.
+    det_return <- det_dataframe %>%
+      select(
+        datecollected,
+        tagname,
+        collectioncode,
+        commonname,
+        scientificname,
+        detectedby,
+        receiver_group,
+        station,
+        receiver,
+        sensortype,
+        sensorvalue,
+        sensorunit,
+        longitude,
+        latitude
+      ) %>%
+      rename(
+        transmitter_id = tagname,
+        tag_id = catalognumber,
+        #transmitter_deployment_id = catalognumber,
+        tagging_project_name = collectioncode, 
+        species_common_name = commonname,
+        species_scientific_name = scientificname,
+        #CAAB_species_id = I think we're just going to have to set this as null since most of what we're gonna be working with doesn't have one.
+        #WORMS_species_aphia_id = might have to actually call out to WORMS for this? 
+        #animal_sex = derive from tag sheet, null if can't
+        detection_datetime = datecollected,
+        installation_name = receiver_group,
+        station_name = station,
+        receiver_id = receiver,
+        receiver_name = ,
+        receiver_deployment_id = ,
+      ) %>%
+      mutate(
+        CAAB_species_id = NA,
+        WORMS_species_aphia_id = NA,
+        animal_sex = NA
+      )
+  }
+  
+  if(is.null(rcvr_dataframe)) {
+    rcvr_dataframe <- derive_rcvr_from_det(det_dataframe)
+  }
+  
+  if(is.null(tag_dataframe)) {
+    tag_dataframe <- derive_tag_from_det(det_dataframe)
+  }
+  #Write in functions for deriving minimal tag/receiver dataframes from the detection extract
+  
+  #Old version commented so I don't have to do all the mapping again. 
+  # if(!is.null(det_dataframe)) {
+  #   #We need to start by renaming the columns containing the appropriate data to the equivalent names that imos uses.
+  #   dd <- det_dataframe %>%
+  #     select(
+  #       datecollected,
+  #       tagname,
+  #       collectioncode,
+  #       commonname,
+  #       scientificname,
+  #       detectedby,
+  #       receiver_group,
+  #       station,
+  #       receiver,
+  #       sensortype,
+  #       sensorvalue,
+  #       sensorunit,
+  #     ) %>%
+  #     rename(
+  #       detection_datetime = datecollected,
+  #       transmitter_id = tagname,
+  #       #tag_id = not sure, derived from tag dataframe?
+  #       #transmitter_deployment_id = same as above
+  #       tagging_project_name = collectioncode, 
+  #       species_common_name = commonname,
+  #       species_scientific_name = scientificname,
+  #       #CAAB_species_id = I think we're just going to have to set this as null since most of what we're gonna be working with doesn't have one.
+  #       #WORMS_species_aphia_id = might have to actually call out to WORMS for this? 
+  #       #animal_sex = derive from tag sheet
+  #       receiver_project_name = detectedby,
+  #       installation_name = receiver_group,
+  #       station_name = station,
+  #       receiver_id = receiver,
+  #       #receiver_deployment_longitude = same
+  #       #receiver_deployment_latitude = same
+  #       transmitter_sensor_type = sensortype,
+  #       transmitter_sensor_value = sensorvalue,
+  #       transmitter_sensor_unit = sensorunit,
+  #       #transmitter_sensor_slope = , I don't think we store this information anywhere
+  #       #transmitter_sensor_intercept = ,
+  #       #transmitter_type = , Derived, I think, from tagmodel, as with all the ones below.
+  #       #transmitter_serial_number = ,
+  #       #transmitter_estimated_battery_life= ,
+  #       #transmitter_status = ,
+  #       #transmitter_deployment_longitude = ,
+  #       #transmitter_deployment_latitude = ,
+  #       #transmitter_deployment_datetime = ,
+  #       #transmitter_dual_sensor = ,
+  #     )
+  # }
+  
+  #If we have ONLY detections, we need to scuff together dataframes for receivers and tags that Remora will like.
+}
+
+#Hack together a piecemeal receiver metadata dataframe for instances where we get detection data, no receiver/tag metadata, but still want to act
+#as though we DID get receiver/tag metadata.
+derive_rcvr_from_det <- function(det_dataframe) {
+  rcvr <- det_dataframe %>%
+    select(
+      #Select the columns from the detection extract that we have access to.
+      detectedby,
+      station,
+      receiver,
+      receiver_depth,
+      longitude,
+      latitude
+    ) %>%
+    rename (
+      #Rename those columns to fit the imos spec
+      installation_name = detectedby,
+      station_name = station,
+      receiver_name = receiver,
+      depth_below_surface = receiver_depth,
+      receiver_deployment_longitude = longitude,
+      receiver_deployment_latitude = latitude
+    ) %>%
+    mutate (
+      #Add NA-filled columns for anything that can't be derived.
+      receiver_deployment_id = NA,
+      purchasing_organisation = NA,
+      receiver_project_name = NA,
+      receiver_status = NA, 
+      receiver_deployment_datetime = NA,
+      receiver_recovery_datetime = NA,
+      receiver_recovery_longitude = NA,
+      receiver_recovery_latitude = NA
+    )
+  
+  return(rcvr)
+  
+  
+}
+
+derive_tag_from_det <- function(det_dataframe) {
+  tag <- det_dataframe %>%
+    select(
+      
+    ) %>%
+    rename (
+      
+    ) %>%
+    mutate (
+      
+    )
+  
+  return(tag)
+}
