cb816d6392be8ae80305639e1eabc8be5799a815
Bruce Delo <bruce@Bruces-MacBook-Pro-579.local>
2023-06-09 13:55:31 -0300

    Adding a temporary cast for shapefile crop so that over can process it hopefully.

--- R/qc_release_location_test.R
+++ R/qc_release_location_test.R
@@ -4,7 +4,7 @@ qc_release_location_test <- function(data, qc_result, species_range, distances, 
   if(!is.null(species_range)) {
     message("We have a shapefile")
     qc_result[, "ReleaseLocation_QC"] <- ifelse(distances[1] > distance_threshold &
-                                                         sum(is.na(sp::over(latlons, species_range))) > 0, 2, 1)
+                                                         sum(is.na(sp::over(latlons, as(species_range, 'Spatial')))) > 0, 2, 1)
   } else {
     message("We have no shapefile.")
     qc_result[, "ReleaseLocation_QC"] <- ifelse(distances[1] > distance_threshold, 2, 1)
