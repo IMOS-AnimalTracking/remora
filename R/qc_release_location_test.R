1152907d2c33cd09f9e7c1a86e45001860196c27
Bruce Delo <bruce@Bruces-MacBook-Pro-579.local>
2023-06-12 15:06:00 -0300

    Changed position/method of cast, hopefully it'll work this time.

--- R/qc_release_location_test.R
+++ R/qc_release_location_test.R
@@ -3,8 +3,9 @@ qc_release_location_test <- function(data, qc_result, species_range, distances, 
   ## Release location test
   if(!is.null(species_range)) {
     message("We have a shapefile")
+    species_range_spatial <- as(species_range, 'Spatial')
     qc_result[, "ReleaseLocation_QC"] <- ifelse(distances[1] > distance_threshold &
-                                                         sum(is.na(sp::over(latlons, as(species_range, 'Spatial')))) > 0, 2, 1)
+                                                         sum(is.na(sp::over(latlons, species_range_spatial))) > 0, 2, 1)
   } else {
     message("We have no shapefile.")
     qc_result[, "ReleaseLocation_QC"] <- ifelse(distances[1] > distance_threshold, 2, 1)
