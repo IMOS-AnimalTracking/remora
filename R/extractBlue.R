d384c5e3c25c822f1d1f4afbfa18722884bbcd8a
YuriNiella <42827939+YuriNiella@users.noreply.github.com>
2023-08-15 15:25:06 +1000

    Update extractBlue

--- R/extractBlue.R
+++ R/extractBlue.R
@@ -24,7 +24,8 @@
 ##' Set to `FALSE` to keep it quiet
 ##' @param cache_layers should the downloaded and processed environmental data be cached within
 ##' the working directory? If FALSE (default), the Bluelink data will be stored in a temporary folder 
-##' and discarded after environmental extraction
+##' and discarded after environmental extraction. Otherwise, it will be saved in the "cached" folder
+##' within folder_name. 
 ##' @param fill_gaps should the function use a spatial buffer to estimate 
 ##' environmental variables for detections where there is missing data. Default 
 ##' is `FALSE` to save computational time.
@@ -86,7 +87,6 @@
 ##'                datetime = "detection_datetime", 
 ##'                env_var = "ocean_temp",
 ##'                extract_depth = 0,
-##'                cache_layers = FALSE,
 ##'                folder_name = "Bluelink",
 ##'                verbose = TRUE,
 ##'                fill_gaps = FALSE)
@@ -98,161 +98,242 @@
 ##' @export
 ##'
  
-extractBlue <- function(df, X = "longitude", Y = "latitude", datetime = "detection_timestamp", env_var, extract_depth = 0, 
-  var_name = paste(env_var, extract_depth, sep = "_"), folder_name = "Bluelink", verbose = TRUE, cache_layers = FALSE, 
-  full_timeperiod = FALSE, station_name, fill_gaps = FALSE, buffer = 10000, 
+extractBlue <- function(df, X, Y, datetime, env_var, extract_depth = 0, 
+  var_name = paste(env_var, extract_depth, sep = "_"), folder_name = "Bluelink", 
+  verbose = TRUE, cache_layers = FALSE, full_timeperiod = FALSE, station_name = NULL, 
+  fill_gaps = FALSE, buffer = 10000, 
   export_step = TRUE, export_path = "Processed_data") {
   
-  ## Initial checks of parameters
+  # Initial checks of parameters
+  if (full_timeperiod & export_step){stop("'export_step' option is currently not available when full_timeperiod = TRUE. Please set export_step = FALSE.")}
   if(!X %in% colnames(df)){stop("Cannot find X coordinate in dataset, provide column name where variable can be found")}
   if(!Y %in% colnames(df)){stop("Cannot find Y coordinate in dataset, provide column name where variable can be found")}
+  if(!datetime %in% colnames(df)){stop("Cannot find datetime in dataset, provide column name where variable can be found")}
   if(!datetime %in% colnames(df)){stop("Cannot find date timestamp column in dataset, provide column name where variable can be found")}
   if(!env_var %in% c('ocean_temp', 'ocean_salt', 'ocean_u', 'ocean_v', 'ocean_w', 'ocean_eta_t', 'ocean_mld')){
-      stop("Environmental variable not recognised, options include:\n'ocean_temp', 'ocean_salt', 'ocean_u', 'ocean_v', 'ocean_w', 'ocean_eta_t', 'ocean_mld'")}
+    stop("Environmental variable not recognised, options include:\n'ocean_temp', 'ocean_salt', 'ocean_u', 'ocean_v', 'ocean_w', 'ocean_eta_t', 'ocean_mld'")}
   if(length(env_var) > 1){stop("This function currently only supports extracting a single variable at a time. Please only select one of:\n'ocean_temp', 'ocean_salt', 'ocean_u', 'ocean_v', 'ocean_w', 'ocean_eta_t', 'ocean_mld'")}
   if(is.character(extract_depth)) {
-      stop("Please provide the extract_depth argument as numeric")
+    stop("Please provide the extract_depth argument as numeric")
+  }
+  # Check if data needs to be download for full timeperiod or not
+  df <- as.data.frame(df)
+  if (full_timeperiod) {
+    if (is.null(station_name)) {
+      stop("Please provide column with station names in the 'station_name' argument.")
+    } else {
+      # Create empty dataset to be filled
+      stations <- unique(df[ ,station_name])
+      all.dates <- as.Date(range(unique(df[,datetime])), tz = "UTC")
+      all.dates <- seq(all.dates[1], all.dates[2], 1)
+      df.all <- expand.grid(all.dates, stations)
+      names(df.all) <- c("date", station_name)
+      df.all$lon <- df[,X][match(df.all[,station_name], df[,station_name])]
+      df.all$lat <- df[,Y][match(df.all[,station_name], df[,station_name])]
+      df.all$Var <- NA 
+      names(df.all)[length(names(df.all))] <- var_name
+        # For data download!
+        df.all$aux.date <- substr(df.all$date, 1, 7) 
+        dates <- unique(df.all$aux.date)
+      if (dir.exists(folder_name) == FALSE) 
+        dir.create(folder_name)
     }
-
-    # Find time interval of interest to download data (years and months)
-    index.col <- which(names(df) == var_name)
-    if (length(index.col) == 0) {
+  } else {
+    # Check for previous function runs
+    if (dir.exists(folder_name) == FALSE) 
+      dir.create(folder_name)
+    df$aux.date <- substr(df[,which(names(df) == datetime)], 1, 7)
+    if (var_name %in% names(df)) {
+      message(paste("The", var_name, "variable was found. Continuing data download."))
+      dates <- unique(df$aux.date[is.na(df[which(names(df) == var_name)])])  
+    } else {
+      dates <- unique(df$aux.date)
       df$Var <- NA
       names(df)[length(names(df))] <- var_name
-      index.row <- 1
-    } else { # If variable extraction was aptempted before
-      index.row <- which(is.na(df[,index.col]))[1]
     }
-    year.month <- unique(substr(as.data.frame(df[(index.row:nrow(df)),which(names(df) == datetime)])[,1], 1, 7))
-    year.month <- str_split(year.month, pattern = "-")
-
-    ### Download environmental data
-    if (verbose) {
-      message(paste("Downloading and processing Bluelink BRAN data:", env_var))
-    pb <-  txtProgressBar(min = 0, max = length(year.month), initial = 0, style = 3, width = 60)
-    }
-  for (dates.index in 1:length(year.month)) {
-    # Create temporaty derectory to save netCDF files (if it doesn't already exist)
-      if(dir.exists(folder_name) == FALSE)
-        dir.create(folder_name) 
-      ### Download data
-    year <- year.month[[dates.index]][1]
-    month <- year.month[[dates.index]][2]
+  }
+  # Download and extract variable:
+  if (verbose) {
+    message(paste("Downloading and processing", var_name, "data:",
+      min(dates), "|", max(dates)))
+    pb <-  txtProgressBar(min = 0, max = length(dates), initial = 0, style = 3, width = 60)
+  }
+  for (i in 1:length(dates)) {
+    # Download BRAN data
     options(timeout = 1000000000) # Increase timeout for slow internet connections
-    download.file(paste0("https://dapds00.nci.org.au/thredds/fileServer/gb6/BRAN/BRAN2020/daily/", env_var, "_", year, "_", month, ".nc"), 
-        destfile = paste0(folder_name, "/", env_var, "_", year, "_", month, ".nc"), mode = 'wb', quiet = TRUE)        
-    ### Process environmental data
-    bran.depths <- c(2.5, 7.5, 12.5, 17.51539, 22.66702, 28.16938, 34.21801, 40.95498, 48.45498, 56.71801, 65.66938, 
-      75.16702, 85.01539, 95, 105, 115, 125, 135, 145, 155, 165, 175, 185, 195, 205.1899, 217.0545, 233.1943, 255.8842, 
-        286.609, 325.8842, 373.1943, 427.0545, 485.1899, 545.5111, 610.4156, 685.9268, 775.9268, 880.4156, 995.5111, 
-        1115.313, 1238.354, 1368.157, 1507.734, 1658.157, 1818.354, 1985.313, 2165.18, 2431.101, 2894.842, 3603.101, 4509.18)
-    bran.layer <- which.min(abs(bran.depths - extract_depth)) # Find layer of interest
-    tot.dates <- unique(as.Date(df[,which(names(df) == datetime)]))
-    if (full_timeperiod) 
-      tot.dates <- seq(min(tot.dates), max(tot.dates), 1)
-    # Define study area
-    aux.r <- terra::rast()
-    terra::ext(aux.r) <- c(
-      min(df[,which(names(df) == X)]) - 1,
-        max(df[,which(names(df) == X)]) + 1,
-        min(df[,which(names(df) == Y)]) - 1,
-        max(df[,which(names(df) == Y)]) + 1)
-    # Help objects to select Bluelink layer of interest
-    aux.names <- list.files(folder_name, pattern = ".nc")
-    nc.help <- terra::rast(paste(folder_name, aux.names, sep = "/"))
-    days.help <- dim(nc.help)[3]/51 # Number of days of data
-    df.name <- expand.grid(Depths = bran.depths, Time = seq(1, days.help, 1))
-    aux.layers <- which(df.name$Depths == df.name$Depths[bran.layer])
-    # Load and combine netCDFs (only days of interest)
-    aux.nc <- terra::rast(paste(folder_name, aux.names, sep = "/"),
-      lyrs = aux.layers, 
-      win = terra::ext(aux.r))
-    # Auxiliar object to find dates in the file
-    aux.dates <- seq(
-      as.Date(paste(
-          substr(stringr::str_remove(aux.names, pattern = env_var), 2, 5), 
-            substr(stringr::str_remove(aux.names, pattern = env_var), 7, 8),
-            1, sep = "-")),
-        as.Date(paste(
-            substr(stringr::str_remove(aux.names, pattern = env_var), 2, 5), 
-            substr(stringr::str_remove(aux.names, pattern = env_var), 7, 8),
-            1, sep = "-")) + (length(names(aux.nc)) - 1), 
-        1
-    ) 
-    # Match days of data to tracking data
-    index <- which(aux.dates %in% tot.dates)
-    aux.nc <- aux.nc[[index]]
-    time.names <- aux.dates[index] # For renaming netCDF layers! 
-    ### Extract environmental data from detection data (daily run)   
-    if (full_timeperiod) {
-      aux.stations <- unique(df[,which(names(df == station_name))])
-        df.aux <- tibble(station_name = aux.stations)
-        df.aux <- expand.grid(station_name = df.aux$station_name, 
-          detection_datetime = tot.dates)
-        df.aux$Y <- df[,which(names(df) == Y)][match(df.aux$station_name, df[,which(names(df) == station_name)])]
-        df.aux$X <- df[,which(names(df) == X)][match(df.aux$station_name, df[,which(names(df) == station_name)])]
-
-        # Find days with detections per station
-        df.aux$Detection <- 0
-        df$aux <- paste(df[,which(names(df) == station_name)], as.Date(df[,which(names(df) == datetime)]), sep = "_")
-        df.aux$aux <- paste(df.aux$station_name, df.aux$detection_datetime, sep = "_")
-        df.aux$Detection[df.aux$aux %in% df$aux] <- 1
-        df.aux <- df.aux[,-6]
-        df.aux$Var <- NA
-        df <- tibble::tibble(df.aux)
-    } 
-    # Find dates of interest to extract data
-    for (i in 1:length(time.names)) {
-      index <- which(as.Date(df[,which(names(df) == datetime)]) == time.names[i])
-        aux.locs <- df[index, c(X, Y)]
-        env.vals <- round(terra::extract(x = aux.nc[[i]], y = aux.locs), 3)
-        env.vals <- env.vals[,2]
-        # Fill gaps for missing data
-        if (fill_gaps) {
-          index2 <- which(is.na(env.vals))
-            if (length(index2) > 0) {
-              # Create point object and apply buffer
-            pos_sf <- 
-            aux.locs[index2,] %>% 
-            sf::st_as_sf(coords = c(1,2), crs = 4326, remove = FALSE)
-          pos_sf <- sf::st_buffer(pos_sf, buffer)
-          # Extract data from buffered points
-              env.vals2 <- terra::extract(x = aux.nc[[i]], y = pos_sf)
-              names(env.vals2)[2] <- "Buffer"
-              env.vals2 <- env.vals2 %>%
-                group_by(ID) %>%
-                summarise(Buffer_mean = mean(Buffer, na.rm = TRUE))
-                env.vals2 <- round(env.vals2$Buffer_mean, 3)  
-              env.vals[index2] <- env.vals2
-            }
-        }
-        df[index, which(names(df) == var_name)] <- env.vals
+    aux.date <- dates[i]
+    dataDownload(type = "day", 
+      year = as.numeric(substr(aux.date, 1, 4)), 
+      month = as.numeric(substr(aux.date, 6, 7)), 
+      dir = folder_name, 
+      varname = env_var,
+      quiet = TRUE)
+    # Load BRAN data
+    nc.bran <- terra::rast(
+      paste0(folder_name, "/", 
+      env_var, "_", substr(aux.date, 1, 4), "_", substr(aux.date, 6, 7), ".nc")) 
+    # Auxiliar object to find depth and day of interest (for 3D variables)
+    if (env_var == "ocean_temp")
+      aux.names <- str_remove(names(nc.bran), pattern = "temp_st_ocean=")
+    if (env_var == "ocean_salt")
+      aux.names <- str_remove(names(nc.bran), pattern = "salt_st_ocean=")
+    if (env_var == "ocean_v")
+      aux.names <- str_remove(names(nc.bran), pattern = "v_st_ocean=")
+    if (env_var == "ocean_u")
+      aux.names <- str_remove(names(nc.bran), pattern = "u_st_ocean=")
+    if (env_var == "ocean_eta_t")
+      aux.names <- str_remove(names(nc.bran), pattern = "eta_t_")
+    if (env_var == "ocean_mld")
+      aux.names <- str_remove(names(nc.bran), pattern = "mld_")
+    aux.names <- str_remove(aux.names, pattern = "Time=")
+    aux.names <- str_split(aux.names, pattern = "_")          
+    if (env_var %in% c("ocean_temp", "ocean_salt", "ocean_v", "ocean_u", "ocean_w")) {
+      aux.depth <- NULL
+    } else {
+      aux.depth <- 0
     }
-    if (export_step)
-      write.csv(df, paste0(export_path, ".csv"), row.names = FALSE) # Export extracted dataset!
-    ### Save data download and processed?
-    if (cache_layers) {
-      if (dir.exists(paste(folder_name, "Processed", sep = "/")) == FALSE) 
-        dir.create(paste(folder_name, "Processed", sep = "/")) 
-      # Export netCDF to file:
-      terra::time(aux.nc) <- time.names
-      suppressWarnings(terra::writeCDF(x = aux.nc, 
-          filename = paste0(folder_name, "/Processed/Bluelink_", env_var, "_", extract_depth, "_", year, "_", month, ".nc"), 
-            overwrite = TRUE, varname = var_name, zname = "days since 1970-01-01"))
-    } 
-    # Remove raw Bluelink files (too large - global res)
+    aux.time <- NULL
+    for (size.depth in 1:length(aux.names)) {
+      if (env_var %in% c("ocean_temp", "ocean_salt", "ocean_v", "ocean_u", "ocean_w")) {
+        aux.depth <- c(aux.depth, aux.names[[size.depth]][1])
+        aux.time <- c(aux.time, aux.names[[size.depth]][2]) 
+      } else {
+        aux.time <- c(aux.time, aux.names[[size.depth]][1]) 
+      }
+    }
+    aux.names <- data.frame(Depth = as.numeric(aux.depth) * -1, Time = as.numeric(aux.time))
+    aux.names$Time <- as.Date("1979-01-01", tz = "UTC") + aux.names$Time
+    # Process data!
+    if (full_timeperiod) { # Full timeperiod
+      index.day <- which(df.all$aux.date == dates[i])
+      for (ii in 1:length(index.day)) {
+        aux.day <- aux.names[which(as.character(aux.names$Time) == as.character(as.Date(df.all[index.day[ii], "date"], tz = "UTC"))),]
+        index.depth <- which.min(abs(aux.day$Depth - extract_depth))
+        aux.day <- aux.day[index.depth,]
+        if (nrow(aux.day) > 0) {
+          # Extract BRAN values
+          index.layer <- which(aux.names$Depth == aux.day$Depth & aux.names$Time == aux.day$Time)
+          aux.bran <- nc.bran[[index.layer]]
+          aux.val <- terra::extract(
+            x = aux.bran, 
+            y = df.all[index.day[ii], c("lon", "lat")])[1,2]
+          # Export processed data if requested by user
+          if (cache_layers == TRUE) {
+            aux.area <- terra::rast()
+            terra::ext(aux.area) <- c(min(df[,X]), 
+              max(df[,X]), 
+              min(df[,Y]),
+              max(df[,Y]))
+            aux.cache <- terra::crop(x = aux.bran,
+              y = aux.area)
+            terra::writeCDF(aux.cache, 
+              filename = paste0(folder_name, "/cached/", var_name, "_", aux.day$Time[1], ".nc"))
+          }
+          # Use buffer to fill NAs 
+          if (is.na(aux.val) & fill_gaps) {
+            # Create point object and apply buffer
+            pos_sf <- 
+            df.all[index.day[ii], c("lon", "lat")] %>% 
+            sf::st_as_sf(coords = c(1,2), crs = 4326, remove = FALSE)
+            pos_sf <- sf::st_buffer(pos_sf, buffer)
+            # Extract data from buffered points
+            aux.val <- terra::extract(x = aux.bran, y = pos_sf)
+            names(aux.val)[2] <- "Buffer"
+            aux.val <- aux.val %>%
+              group_by(ID) %>%
+              summarise(Buffer_mean = mean(Buffer, na.rm = TRUE))
+            aux.val <- aux.val$Buffer_mean
+            aux.day$Var <- aux.val
+          } else {
+            aux.day$Var <- aux.val
+          }
+        }
+        if (env_var %in% c('ocean_temp', 'ocean_salt', 'ocean_u', 'ocean_v', "ocean_eta_t", "ocean_mld"))
+          aux.day$Var <- round(aux.day$Var, 4)
+        df.all[index.day[ii], which(names(df.all) == var_name)] <- aux.day$Var
+        gc()
+      } 
+    } else {  # Only tracking locations
+      index.day <- which(df$aux.date == dates[i])
+      for (ii in 1:length(index.day)) {
+        aux.day <- aux.names[which(as.character(aux.names$Time) == as.character(as.Date(df[index.day[ii], which(names(df) == datetime)], tz = "UTC"))),]
+        index.depth <- which.min(abs(aux.day$Depth - extract_depth))
+        aux.day <- aux.day[index.depth,]
+        if (nrow(aux.day) > 0) {
+          # Extract BRAN values
+          index.layer <- which(aux.names$Depth == aux.day$Depth & aux.names$Time == aux.day$Time)
+          aux.bran <- nc.bran[[index.layer]]
+          aux.val <- terra::extract(
+            x = aux.bran, 
+            y = df[index.day[ii], c(X, Y)])[1,2]
+          # Export processed netCDF if requested by user
+          if (cache_layers == TRUE) {
+            if (dir.exists(paste(folder_name, "cached", sep = "/")) == FALSE) 
+              dir.create(paste(folder_name, "cached", sep = "/"))
+            aux.area <- terra::rast()
+            terra::ext(aux.area) <- c(min(df[,X]), 
+              max(df[,X]), 
+              min(df[,Y]),
+              max(df[,Y]))
+            aux.cache <- terra::crop(x = aux.bran,
+              y = aux.area)
+            terra::writeCDF(aux.cache, 
+              filename = paste0(folder_name, "/cached/", var_name, "_", aux.day$Time[1], ".nc"),
+              overwrite = TRUE)
+          }
+          # Use buffer to fill NAs 
+          if (is.na(aux.val) & fill_gaps) {
+            # Create point object and apply buffer
+            pos_sf <- 
+            df[index.day[ii], c(X, Y)] %>% 
+            sf::st_as_sf(coords = c(1,2), crs = 4326, remove = FALSE)
+            pos_sf <- sf::st_buffer(pos_sf, buffer)
+            # Extract data from buffered points
+            aux.val <- terra::extract(x = aux.bran, y = pos_sf)
+            names(aux.val)[2] <- "Buffer"
+            aux.val <- aux.val %>%
+              group_by(ID) %>%
+              summarise(Buffer_mean = mean(Buffer, na.rm = TRUE))
+            aux.val <- aux.val$Buffer_mean
+            aux.day$Var <- aux.val
+          } else {
+            aux.day$Var <- aux.val
+          }
+        }
+        if (env_var %in% c('ocean_temp', 'ocean_salt', 'ocean_u', 'ocean_v', "ocean_eta_t", "ocean_mld"))
+          aux.day$Var <- round(aux.day$Var, 3)
+        df[index.day[ii], which(names(df) == var_name)] <- aux.day$Var
+        gc()
+      } 
+    }
+    if (export_step) # Export extracted data 
+      write.csv(df[,-which(names(df) == "aux.date")], paste0(export_path, ".csv"), row.names = FALSE)
+    # Delete BRAN raw file
     nc.names <- list.files(folder_name, pattern = ".nc")
     nc.names <- paste(folder_name, nc.names, sep = "/")
-    unlink(nc.names)
+    invisible(file.remove(nc.names))
     if (verbose)
-      setTxtProgressBar(pb, dates.index)     
+      setTxtProgressBar(pb, i)     
   }
   if (verbose)
-    close(pb) 
-  if (cache_layers == FALSE) {
-    # Remove entire folder
-    nc.dir <- list.dirs(folder_name)
-    unlink(nc.dir, recursive = TRUE)
+    close(pb)
+  # Match detections and standardized dataset for full_timeperiod runs
+  if (full_timeperiod) {
+    df.all$aux.date <- paste(df.all[,1], df.all[,2], sep = "_")
+    df$aux.date <- paste(as.Date(df[,which(names(df) == datetime)], tz = "UTC"), 
+      df[,station_name], sep = "_")
+    index <- which(df.all$aux.date %in% df$aux.date)
+    df.all$Detection <- 0
+    df.all$Detection[index] <- 1 # Days and locations with detections!
+    df.all <- df.all[,-which(names(df.all) == "aux.date")]
   }
-  return(df)
+  # Delete temporary directory
+  if (cache_layers == FALSE) 
+    invisible(unlink(folder_name, recursive = TRUE))
+  # Export results
+  if (full_timeperiod) {
+    return(df.all)
+  } else {
+    return(df)
+  } 
 }
